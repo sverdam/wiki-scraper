0b4f7a5415f14b215e5eeaa8e9a2431a53d9f5de
snwflwrsoph <asophia.peraltar@gmail.com>
2026-09-01 15:17:25 -0700

    Profile

--- scraper.py
+++ scraper.py
@@ -1,11 +1,38 @@
 # wuu web scraper para undertale 
 
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import datetime
 import random
 import re
+from urllib.error import HTTPError, URLError
 
+
+url = "https://deltarune.wiki/"
+
+try:
+    req = Request(
+        url,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/139.0.0.0 Safari/537.36"
+        }
+    )
+
+    html = urlopen(req)
+    bs = BeautifulSoup(html.read(), "html.parser")
+
+    print(bs.h1)
+
+except HTTPError as e:
+    print("HTTP Error:", e.code)
+
+except URLError as e:
+    print("URL Error:", e.reason)
+
+except Exception as e:
+    print("Something else went wrong:", e)
 
 
 """
