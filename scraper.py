cf3a333a04f9d1e551679776cfe296b02dd46053
snwflwrsoph <asophia.peraltar@gmail.com>
2026-09-01 15:09:24 -0700

    First Commit - Web Scraper

---
+++ scraper.py
@@ -0,0 +1,34 @@
+# wuu web scraper para undertale 
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import datetime
+import random
+import re
+
+
+
+"""
+html = urlopen('http://pythonscraping.com/pages/page1.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+print(bs.h1)
+"""
+"""
+html2 = urlopen('https://imagine.gsfc.nasa.gov/science/objects/neutron_stars1.html')
+bs2 = BeautifulSoup(html2, 'html.parser')
+for link in bs2.find_all('a'):
+    if 'href' in link.attrs:
+        print(link.attrs['href'])
+"""
+
+#random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html3 = urlopen('https://imagine.gsfc.nasa.gov{}'.format(articleUrl))
+    bs3 = BeautifulSoup(html3, 'htmlparser')
+    return bs3.find('div').find_all('a', href = re.compile('^/science/)((?!:).)*$'))
+
+links = getLinks('/science/objects/neutron_stars1.html')
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+    print(newArticle)
+    links.getLinks(newArticle)
