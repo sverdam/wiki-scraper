884c54313b1fec320400de1afb654fdc0ec136b2
snwflwrsoph <asophia.peraltar@gmail.com>
2026-09-01 22:57:38 -0700

    feat: undertale scraper that works!!

--- scraper.py
+++ scraper.py
@@ -8,7 +8,6 @@ import re
 from urllib.parse import urljoin
 from urllib.error import HTTPError, URLError
 
-
 url = "https://deltarune.wiki/"
 
 def getLinks(aUrl):
@@ -37,47 +36,3 @@ while len(links) > 0:
 
         url = newArticle
         links = getLinks(url)
-
-
-
-
-
-
-"""
-try:
-    
-
-    
-    def getLinks(aUrl):
-        
-        
-
-    
-    
-
-
-    print(bs.h1)
-
-except HTTPError as e:
-    print("HTTP Error:", e.code)
-
-except URLError as e:
-    print("URL Error:", e.reason)
-
-except Exception as e:
-    print("Something else went wrong:", e)
-"""
-
-"""
-def getLinks(articleUrl):
-    html = urlopen(url.format(articleUrl))
-    bs = BeautifulSoup(html, 'htmlparser')
-    return bs.find('div').find_all('a')
-
-links = getLinks('https://deltarune.wiki/')
-while len(links) > 0:
-    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
-    print(newArticle)
-    links.getLinks(newArticle)
-
-"""
